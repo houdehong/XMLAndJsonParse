80e1fd75eef1cd6a26cdf52b2b4e209edae0b864
houdehog <2948656997@qq.com>
2019-05-14 16:51:59 +0800

    jdom和dom4j的解析

--- src/XMLParse/Dom4j.java
+++ src/XMLParse/Dom4j.java
@@ -1,5 +1,15 @@
 package XMLParse;
 
+import org.dom4j.Document;
+import org.dom4j.DocumentHelper;
+import org.dom4j.Element;
+import org.dom4j.io.OutputFormat;
+import org.dom4j.io.SAXReader;
+import org.dom4j.io.XMLWriter;
+
+import java.io.*;
+import java.util.List;
+
 /**
  * @author houdehong
  * @date 2019/5/13
@@ -8,4 +18,95 @@ package XMLParse;
  * @since NetSignServer_5.5.40.12_patch
  */
 public class Dom4j {
+    public static void main(String[] args) {
+        //generateXml();
+        parseXml();
+    }
+    private static void generateXml(){
+        try {
+            //DocumentHelper提供了创建Document对象的方法
+            Document document = DocumentHelper.createDocument();
+            //添加节点信息
+            Element rootElement = document.addElement("modules");
+            //这里可以继续添加子节点，也可以指定内容
+            rootElement.setText("这个是module标签的文本信息");
+            Element element = rootElement.addElement("module");
+
+            Element nameElement = element.addElement("name");
+            Element valueElement = element.addElement("value");
+            Element descriptionElement = element.addElement("description");
+            nameElement.setText("名称");
+            //为节点添加属性值
+            nameElement.addAttribute("language", "java");
+            valueElement.setText("值");
+            valueElement.addAttribute("language", "c#");
+            descriptionElement.setText("描述");
+            descriptionElement.addAttribute("language", "sql server");
+            // 设置XML文档格式
+            OutputFormat outputFormat = OutputFormat.createPrettyPrint();
+            // 设置XML编码方式,即是用指定的编码方式保存XML文档到字符串(String),这里也可以指定为GBK或是ISO8859-1
+            outputFormat.setEncoding("GBK");
+            //outputFormat.setSuppressDeclaration(true); //是否生产xml头
+            //设置是否缩进
+            outputFormat.setIndent(true);
+            //以四个空格方式实现缩进
+            outputFormat.setIndent("    ");
+            //设置是否换行
+            outputFormat.setNewlines(true);
+
+            //将document文档对象直接转换成字符串输出
+            System.out.println(document.asXML());
+            Writer fileWriter = new FileWriter("src/dom4j/gen.xml");
+            //dom4j提供了专门写入文件的对象XMLWriter
+            XMLWriter xmlWriter = new XMLWriter(fileWriter,outputFormat);
+            xmlWriter.write(document);
+            xmlWriter.flush();
+            xmlWriter.close();
+            System.out.println("xml文档添加成功！");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private static void parseXml(){
+        try{
+            //将src下面的xml转换为输入流
+            //InputStream inputStream = this.getClass().getResourceAsStream("src/dom4j/module03.xml");
+            //创建SAXReader读取器，专门用于读取xml
+            SAXReader saxReader = new SAXReader();
+            //根据saxReader的read重写方法可知，既可以通过inputStream输入流来读取，也可以通过file对象来读取
+            Document document = saxReader.read(new File("src/dom4j/module03.xml"));
+            Element rootElement = document.getRootElement();
+            if(rootElement.elements("module") != null ){
+                //因为第一个module标签只有内容没有子节点，直接.iterator()就java.lang.NullPointerException了, 所以需要分开实现
+                List<Element> elementList = rootElement.elements("module");
+                for (Element element : elementList) {
+                    if(!element.getTextTrim().equals("")){
+                        System.out.println("【1】" + element.getTextTrim());
+                    }else{
+                        Element nameElement = element.element("name");
+                        System.out.println("   【2】" + nameElement.getName() + ":" + nameElement.getText());
+                        Element valueElement = element.element("value");
+                        System.out.println("   【2】" + valueElement.getName() + ":" + valueElement.getText());
+                        Element descriptElement = element.element("descript");
+                        System.out.println("   【2】" + descriptElement.getName() + ":" + descriptElement.getText());
+                        List<Element> subElementList = element.elements("module");
+                        for (Element subElement : subElementList) {
+                            if(!subElement.getTextTrim().equals("")){
+                                System.out.println("      【3】" + subElement.getTextTrim());
+                            }else{
+                                Element subnameElement = subElement.element("name");
+                                System.out.println("      【3】" + subnameElement.getName() + ":" + subnameElement.getText());
+                                Element subvalueElement = subElement.element("value");
+                                System.out.println("      【3】" + subvalueElement.getName() + ":" + subvalueElement.getText());
+                                Element subdescriptElement = subElement.element("descript");
+                                System.out.println("      【3】" + subdescriptElement.getName() + ":" + subdescriptElement.getText());
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/XMLParse/JDom.java
+++ src/XMLParse/JDom.java
@@ -1,5 +1,18 @@
 package XMLParse;
 
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * @author houdehong
  * @date 2019/5/13
@@ -8,4 +21,100 @@ package XMLParse;
  * @since NetSignServer_5.5.40.12_patch
  */
 public class JDom {
+    public static void main(String[] args) {
+        try {
+            File file = new File("src/JDom/airportLine.xml");
+            SAXBuilder builder = new SAXBuilder();
+            Document doc = builder.build(file);
+            // 解析XML文档
+            //parseJDOM(doc);
+            // 生成xml文档
+            String[] ipAddress = {"172.16.0.1","172.16.0.2"};
+            String[] testAddress = {"192.168.1.0/24","192.168.2.0/24"};
+            System.out.println("生成 mxl 文件...");
+            BuildXMLDoc(2, ipAddress, testAddress);
+        } catch (Exception e) {
+            System.out.println("Can't read the file");
+        }
+    }
+
+    // 解析XML文档
+    private static void parseJDOM(Document doc) {
+        Element root = doc.getRootElement();
+        // 也可使用root.getChildren()
+        List lineList = root.getChildren("line");
+        for (Iterator iter = lineList.iterator(); iter.hasNext();) {
+            // 获取<line>元素
+            Element lineElement = (Element) iter.next();
+            // 获取<line>元素的lid属性值
+            String lid = lineElement.getAttributeValue("lid");
+            // 获取<line>元素的num属性值
+            String num = lineElement.getAttributeValue("num");
+
+            System.out.println("==lid:" + lid);
+            System.out.println("==num:" + num);
+
+            // 获得<line>下<id>标签下的子元素
+            Element idElement = lineElement.getChild("id");
+            // // 获得<line>下<id>标签下的子元素值
+            String id = idElement.getText();
+            System.out.println("==路线id:" + id);
+            // 获得<line>下<station>列表
+            List stationList = lineElement.getChildren("station");
+            for (Iterator subIter = stationList.iterator(); subIter.hasNext();) {
+                // 获取<station>元素
+                Element stationElement = (Element) subIter.next();
+                // 获得<station>下<sid>标签下的子元素
+                Element sidElement = stationElement.getChild("sid");
+                // 获得<station>下<sname>标签下的子元素
+                Element snameElement = stationElement.getChild("sname");
+                // 获得<station>下<sid>标签下的子元素值
+                String sid = sidElement.getText();
+                // 获得<station>下<sname>标签下的子元素值
+                String sname = snameElement.getText();
+
+                System.out.println("==路线sid:" + sid);
+                System.out.println("==路线sname:" + sname);
+            }
+        }
+    }
+    private static void BuildXMLDoc(int number, String ipAddress[], String testAddress[]) throws IOException, JDOMException {
+        //建立根节点
+        Element equipments = new Element("equipments");
+        //用把根节点创建一个Document
+        Document doc = new Document(equipments);
+        //创建根节点下的子节点
+        Element equipmentsNum = new Element("equipments-number");
+        equipmentsNum.setText(Integer.toString(number));
+        //把该子节点加入父节点
+        equipments.addContent(equipmentsNum);
+        //循环创建子节点，并加入到父节点中
+        for (int i = 1; i <= number; i++) {
+            Element equipment = new Element("equipment");
+            //添加节点属性并赋值
+            equipment.setAttribute("id", "" + i);
+
+            Element loginInfo = new Element("login-info");
+            //创建节点，给节点赋值，然后加入到他的父节点
+            loginInfo.addContent(new Element("address").setText(ipAddress[i-1]));
+            loginInfo.addContent(new Element("telnet-password").setText("CISCO"));
+            loginInfo.addContent(new Element("enable-password").setText("CISCO"));
+            equipment.addContent(loginInfo);
+
+            Element testCommands = new Element("test-commands");
+            testCommands.addContent(new Element("command").setText("show ip route"));
+            equipment.addContent(testCommands);
+
+            Element testReturn = new Element("test-returns");
+            testReturn.addContent(new Element("return").setText(testAddress[i-1]));
+            equipment.addContent(testReturn);
+
+            equipments.addContent(equipment);
+        }
+        //new一个XMLOutputter，采用PrettyFormat格式（生成的XML可以自动换行）
+        XMLOutputter xmlOut = new XMLOutputter(Format.getPrettyFormat());
+        //将生成的XML文件输出到文件
+        xmlOut.output(doc, new FileOutputStream("src/JDom/autoTest.xml"));
+    }
+
 }
